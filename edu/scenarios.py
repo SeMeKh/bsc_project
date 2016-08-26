cc27b381a697051c0eb18f551f03a1982addbc28
Mehran Kholdi <semekh.dev@gmail.com>
2016-08-26 19:43:19 +0430

    Rename sample checks to scenarios

--- edu/scenarios.py
+++ edu/scenarios.py
@@ -4,57 +4,57 @@ from edu.models import Enrollment, EnrollmentError
 from insanity.scenario import Scenario
 
 
-class EnrollmentCheck1(Scenario):
+class EnrollmentScenario1(Scenario):
     action_name = 'edu.models.Offering.enroll'
 
-    def given(check, self, student, **payload):
-        check.old_capacity = self.available_capacity
+    def given(scenario, self, student, **payload):
+        scenario.old_capacity = self.available_capacity
         return True
 
-    def when(check, commit, **payload):
+    def when(scenario, commit, **payload):
         return commit
 
-    def then(check, payload, return_value, exc_type, **kwargs):
+    def then(scenario, payload, return_value, exc_type, **kwargs):
         assert exc_type is None
-        assert check.old_capacity > 0
-        assert payload['self'].available_capacity == check.old_capacity - 1
+        assert scenario.old_capacity > 0
+        assert payload['self'].available_capacity == scenario.old_capacity - 1
         assert isinstance(return_value, Enrollment)
         print('checked1')
 
 
-class EnrollmentCheck2(Scenario):
+class EnrollmentScenario2(Scenario):
     action_name = 'edu.models.Offering.enroll'
 
-    def given(check, self, **payload):
+    def given(scenario, self, **payload):
         return self.available_capacity == 0
 
-    def then(check, exc_type, **kwargs):
+    def then(scenario, exc_type, **kwargs):
         assert issubclass(exc_type, EnrollmentError)
         print('checked2')
 
 
-class EnrollmentCheck3(Scenario):
+class EnrollmentScenario3(Scenario):
     action_name = 'edu.models.Offering.enroll'
 
-    def then(check, payload, **kwargs):
+    def then(scenario, payload, **kwargs):
         offering = payload['self']
         assert offering.capacity - offering.available_capacity == Enrollment.objects.filter(offering=offering).count()
         print('checked3')
 
 
-class SampleContextCheck4(Scenario):
+class SampleContextScenario4(Scenario):
     action_name = 'offeringCapacityChangeByStaff'
 
-    def then(check, payload, **kwargs):
+    def then(scenario, payload, **kwargs):
         offering = payload['offering']
         assert offering.capacity - offering.available_capacity == Enrollment.objects.filter(offering=offering).count()
         print('checked4')
 
 
-class SampleContextCheck5(Scenario):
+class SampleContextScenario5(Scenario):
     action_name = 'offeringCapacityChangeByStaff'
 
-    def then(check, payload, **kwargs):
+    def then(scenario, payload, **kwargs):
         offering = payload['offering']
         assert not offering.is_enrollable
         print('checked5')
