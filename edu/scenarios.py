3dce8dd751cb289cb4479bc73ea7774b375cb3e9
Mehran Kholdi <semekh.dev@gmail.com>
2016-08-26 22:14:57 +0430

    Change code for thesis

--- edu/scenarios.py
+++ edu/scenarios.py
@@ -7,22 +7,29 @@ from insanity.scenario import Scenario
 class EnrollmentScenario1(Scenario):
     action_name = 'edu.models.Offering.enroll'
 
-    def given(scenario, self, student, **payload):
-        scenario.old_capacity = self.available_capacity
+    def given(scenario, o1, s2, **payload):
+        scenario.old_capacity = o1.available_capacity
         return True
 
     def when(scenario, commit, **payload):
         return commit
 
     def then(scenario, payload, return_value, exc_type, **kwargs):
-        assert exc_type is None
         assert scenario.old_capacity > 0
         assert payload['self'].available_capacity == scenario.old_capacity - 1
-        assert isinstance(return_value, Enrollment)
-        print('checked1')
 
 
 class EnrollmentScenario2(Scenario):
+    action_name = 'edu.models.Offering.enroll'
+
+    def then(scenario, payload, **kwargs):
+        o1 = payload['self']
+        used_capacity = o1.capacity - o1.available_capacity
+        enrollment_count = Enrollment.objects.filter(offering=o1).count()
+        assert used_capacity == enrollment_count
+
+
+class EnrollmentScenario3(Scenario):
     action_name = 'edu.models.Offering.enroll'
 
     def given(scenario, self, **payload):
@@ -31,15 +38,6 @@ class EnrollmentScenario2(Scenario):
     def then(scenario, exc_type, **kwargs):
         assert issubclass(exc_type, EnrollmentError)
         print('checked2')
-
-
-class EnrollmentScenario3(Scenario):
-    action_name = 'edu.models.Offering.enroll'
-
-    def then(scenario, payload, **kwargs):
-        offering = payload['self']
-        assert offering.capacity - offering.available_capacity == Enrollment.objects.filter(offering=offering).count()
-        print('checked3')
 
 
 class SampleContextScenario4(Scenario):
