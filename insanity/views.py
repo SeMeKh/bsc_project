02131daefc44abe4c01227eedcca51d8e714c737
Mehran Kholdi <semekh.dev@gmail.com>
2016-08-28 16:57:23 +0430

    Add link to sentry event list

--- insanity/views.py
+++ insanity/views.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from django.shortcuts import render
 
 from insanity.apps import all_stats
 
 
 def report(request):
-    return render(request, 'report.html', {'all_stats': all_stats})
+    return render(request, 'report.html', {'all_stats': all_stats, 'SENTRY_URL': settings.SENTRY_URL})
